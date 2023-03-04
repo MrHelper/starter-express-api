adf59fca476f6093159a3b99f0c30ff1efe34bc1
Hung Nguyen <hung.np988@gmail.com>
2023-03-04 22:26:10 +0700

    update key

--- index.js
+++ index.js
@@ -53,7 +53,8 @@ app.post("/login", jsonParser, async (req, res) => {
 });
 
 app.post("/project", jsonParser, async (req, res) => {
-  let key = req.body.key ? req.body.key : req.body.name + "_" + Date.now();
+  let key =
+    req.body.key == null ? req.body.name + "_" + Date.now() : req.body.key;
   dbpj.set(key, req.body);
   res.send(true);
 });
